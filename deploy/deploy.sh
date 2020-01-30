cb3630536b890b87e662042f05afc99bef186ee4
Vladius25 <vkorol2509@icloud.com>
2020-01-30 17:36:05 +0500

    Info about public key

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,7 +9,7 @@ cd ./REPONAME
 virtualenv --system-site-packages --python=/usr/bin/python3 /opt/REPONAME/PROJECT_NAME/venv
 source PROJECT_NAME/venv/bin/activate
 
-pip install -r /opt/REPONAME/DJANGO_PROJECT/requirements.txt
+pip install --quiet -r /opt/REPONAME/DJANGO_PROJECT/requirements.txt
 
 mkdir -p /var/opt/PROJECT_NAME
 mkdir /var/opt/PROJECT_NAME/media 
@@ -20,8 +20,8 @@ cat ~/deploy/local_settings.py > /opt/REPONAME/PROJECT_NAME/PROJECT_NAME/local_s
 python3 -m compileall -x /opt/REPONAME/PROJECT_NAME/venv/ /opt/REPONAME/PROJECT_NAME 
 
 cd /opt/REPONAME/PROJECT_NAME
-python3 manage.py collectstatic
-python3 manage.py migrate
+python3 manage.py --verbosity 0 collectstatic
+python3 manage.py --verbosity 0 migrate
 
 cd /etc/nginx/sites-available/
 cat ~/deploy/nginx.conf DOMAIN
@@ -37,4 +37,12 @@ cat ~/deploy/systemd.conf > PROJECT_NAME.service
 
 cat  ~/deploy/my_gunicorn  > my_gunicorn
 chmod 755 my_gunicorn
-systemctl enable --now PROJECT_NAME 
+systemctl enable --now PROJECT_NAME
+
+ssh-keygen -t rsa -b 4096 -C "REMOTE_USER@DOMAIN"
+echo "You should set up deploy key, secrets.HOST and secrets.SSH_KEY in you github settins"
+echo "HOST: REMOTE_HOST"
+echo "Public key:"
+cat ~/.ssh/id_rsa.pub
+
+rm -rf ~/deploy
