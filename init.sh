e8fb18482b7f2a458667c87d183e9ba3ff6ed3c3
Vladius25 <vkorol2509@icloud.com>
2020-01-30 17:37:10 +0500

    Windows support and improvements

--- init.sh
+++ init.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 
-# Export variables
-source ENV
+echo "Importing config.ini..."
+source <(grep = config.ini)
+vars=($(grep -aoP ".*(?=\=)" config.ini))
+for var in ${vars[@]}; do
+    if [ -z ${!var} ]; then
+        echo "$var is unset. Edit config.ini"
+        exit 1
+    fi
+done
 
-# Create venv
+echo "Creating venv..."
 mv project $PROJECT_NAME
 cd $PROJECT_NAME
-python3 -mvenv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
+python3 -mvenv venv 2>/dev/null || py -mvenv venv                        #<-- hack for windows
+source venv/bin/activate 2>/dev/null || source venv/Scripts/activate     #<-- hack for windows
+pip3 install --quiet -r requirements.txt
 
-# Rename django project
-python3 manage.py rename project $PROJECT_NAME
+echo "Renaming django project..."
+python manage.py rename project $PROJECT_NAME
 
-# Fix configs
+echo "Seting up configs..."
 cd ..
 for file in `ls deploy`; do
-    sed -i "s/PROJECT_NAME/$PROJECT_NAME/g" deploy/$file 
-    sed -i "s/DOMAIN/$DOMAIN/g" deploy/$file
-    sed -i "s/REPONAME/$REPONAME/g" deploy/$file 
+    for var in ${vars[@]}; do
+        sed -i "s/$var/${!var}/g" deploy/$file
+    done    
 done    
 
-# Deploy on remote and exec
-scp -r deploy $REMOTE_USER@$HOST:~
-scp -r ENV $REMOTE_USER@$HOST:~/deploy/
-ssh $REMOTE_USER@$HOST:~ "bash deploy/deploy.sh"
+echo "Deploying on remote host..."
+scp -r deploy $REMOTE_USER@$REMOTE_HOST:~
+scp -r deploy/update.sh $REMOTE_SUER@$REMOTE_HOST:~
+scp -r config.ini $REMOTE_USER@$REMOTE_HOST:~/deploy/
+ssh $REMOTE_USER@$REMOTE_HOST:~ "bash deploy/deploy.sh"
 
-# Remove trash
-rm -rf deploy ENV init.sh README.md
+echo "Removing temp files..."
+rm -rf deploy config.ini init.sh README.md
 
-# Init README.md
+echo "Creating new README..."
 echo "# $PROJECT_NAME" > README.md
 
-# Init git
+echo "Creating git..."
 rm -rf .git
-git init
-git add -A
-git commit -m "Initial commit"
+git init -q
+git add -A -q
+git commit -m -q "Initial commit"
 git remote add origin $GITHUB_URL
